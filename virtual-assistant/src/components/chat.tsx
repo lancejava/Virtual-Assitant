a6e6207b6b234ff0ca2b23f693f1939775d8381b
Lance javaluyas <lancejavaluyas@gmail.com>
2024-03-09 19:22:50 -0500

    Cleaned up the UI a little bit more. I will be working on the actual API next time.

--- virtual-assistant/src/components/chat.tsx
+++ virtual-assistant/src/components/chat.tsx
@@ -16,23 +16,25 @@ const chat = () => {
         console.log(message)
     }
     return (
-        <div className="bg-[#2B2D35] flex flex-col h-screen w-screen justify-center">  
-            <div className='flex text-lg justify-center items-center flex-col gap-2'>
-                <img src={Logo} alt="Virtual Assistant" className="h-24 w-24" />
-                <h1 className='text-center flex text-24 text-white'>How can I help you today?</h1>
-            </div>
-            <div className=''>
-                <div className="flex p-4 justify-center items-center  bg-#2B2D35">
-                    <input type='text' placeholder='Ask you Virtual Assistant...' 
-                    onChange={(e) => setMessage(e.target.value)}
-                    value={message}/>
-                    <svg onClick={sendMessage} 
-                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-12 h-12 bg-gray p-2 rounded-full">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                    </svg>
-
+        <div className="bg-[#2B2D35] flex border flex-col h-screen w-screen items-center justify-center">  
+            {/* Landing Page / Top Section*/}
+            <div className='h-3/4 w-1/2 border flex p-2 justify-between flex-col items-center'>
+                <div className='flex w-full text-lg h-full justify-center items-center flex-col gap-6'>
+                    <img src={Logo} alt="Virtual Assistant" className="h-24 w-24" />
+                    <h1 className='text-center flex text-xl text-white'>How can I help you today?</h1>
                 </div>
-                <p className='text-center'>I'm only a robot, I too can make mistakes.</p>
+                {/* Bottom Section */}
+                <div className='gap-10 w-full'>
+                    <div className="flex w-full p-4 justify-center items-center gap-2  bg-#2B2D35">
+                        <input className='rounded-full items-center flex p-3 justify-between w-4/5 bg-transparent border border-1 border-gray' type='text' placeholder='Message your Virtual Assistant...' 
+                        onChange={(e) => setMessage(e.target.value)}
+                        value={message}/>
+                        <svg onClick={sendMessage} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="w-12 h-12 bg-transparent rounded-full hover:cursor-pointer hover:shadow-lg transform hover:scale-110 transition duration-300 ease-in-out">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                        </svg>
+                    </div>
+                    <p className='text-center text-white text-xs'>I'm only a robot, I too can make mistakes.</p>
+                </div>
             </div>
         </div>
     )
